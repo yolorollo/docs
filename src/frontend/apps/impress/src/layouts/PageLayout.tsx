66fbf279139fd4451c0c689f6425102d8c391ff1
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2025-02-03 13:56:29 +0100

    🩹(frontend) fix PageLayout
    The page layout was rendered behind the header,
    which caused the top of the mention legales pages
    to be hidden.
    This commit fixes this issue.

--- src/frontend/apps/impress/src/layouts/PageLayout.tsx
+++ src/frontend/apps/impress/src/layouts/PageLayout.tsx
@@ -2,13 +2,18 @@ import { PropsWithChildren } from 'react';
 
 import { Box } from '@/components';
 import { Footer } from '@/features/footer';
-import { Header } from '@/features/header';
+import { HEADER_HEIGHT, Header } from '@/features/header';
+import { LeftPanel } from '@/features/left-panel';
+import { useResponsiveStore } from '@/stores';
 
 export function PageLayout({ children }: PropsWithChildren) {
+  const { isDesktop } = useResponsiveStore();
+
   return (
-    <Box $minHeight="100vh">
+    <Box $minHeight="100vh" $margin={{ top: `${HEADER_HEIGHT}px` }}>
       <Header />
       <Box as="main" $width="100%" $css="flex-grow:1;">
+        {!isDesktop && <LeftPanel />}
         {children}
       </Box>
       <Footer />

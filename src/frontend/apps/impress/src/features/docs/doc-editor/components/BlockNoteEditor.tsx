a0c5a911d733e0a7ed63199730e9c1a95b4c5599
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2025-02-05 09:44:19 +0100

    fix lint

--- src/frontend/apps/impress/src/features/docs/doc-editor/components/BlockNoteEditor.tsx
+++ src/frontend/apps/impress/src/features/docs/doc-editor/components/BlockNoteEditor.tsx
@@ -1,10 +1,10 @@
-import { Box, TextErrors } from '@/components';
-import { useAuthStore } from '@/core/auth';
-import { Doc } from '@/features/docs/doc-management';
 import { createOpenAI } from '@ai-sdk/openai';
 import {
   BlockNoteEditor as BNEditor,
+  BlockConfig,
   Dictionary,
+  InlineContentSchema,
+  StyleSchema,
   filterSuggestionItems,
   locales,
 } from '@blocknote/core';
@@ -12,117 +12,65 @@ import '@blocknote/core/fonts/inter.css';
 import { BlockNoteView } from '@blocknote/mantine';
 import '@blocknote/mantine/style.css';
 import {
-  getDefaultReactSlashMenuItems,
   SuggestionMenuController,
+  getDefaultReactSlashMenuItems,
   useCreateBlockNote,
 } from '@blocknote/react';
 import {
-  locales as aiLocales,
   AIShowSelectionPlugin,
   BlockNoteAIContextProvider,
   BlockNoteAIUI,
+  locales as aiLocales,
   createBlockNoteAIClient,
   getAISlashMenuItems,
   useBlockNoteAIContext,
 } from '@blocknote/xl-ai';
+import '@blocknote/xl-ai/style.css';
 import { HocuspocusProvider } from '@hocuspocus/provider';
-import { useEffect, useMemo } from 'react';
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { css } from 'styled-components';
 import * as Y from 'yjs';
+
+import { Box, TextErrors } from '@/components';
+import { useAuthStore } from '@/core/auth';
+import { Doc } from '@/features/docs/doc-management';
+
 import { useUploadFile } from '../hook';
 import { useHeadings } from '../hook/useHeadings';
 import useSaveDoc from '../hook/useSaveDoc';
 import { useEditorStore } from '../stores';
+import { cssEditor } from '../styles';
 import { randomColor } from '../utils';
 
 import { BlockNoteToolbar } from './BlockNoteToolbar';
 
-const cssEditor = (readonly: boolean) => css`
-  &,
-  & > .bn-container,
-  & .ProseMirror {
-    height: 100%;
+const blocknoteAIClient = createBlockNoteAIClient({
+  apiKey: 'BLOCKNOTE-API-KEY-CURRENTLY-NOT-NEEDED',
+  baseURL: 'https://blocknote-esy4.onrender.com/ai',
+});
 
-    .bn-side-menu[data-block-type='heading'][data-level='1'] {
-      height: 50px;
-    }
-    .bn-side-menu[data-block-type='heading'][data-level='2'] {
-      height: 43px;
-    }
-    .bn-side-menu[data-block-type='heading'][data-level='3'] {
-      height: 35px;
-    }
-    h1 {
-      font-size: 1.875rem;
-    }
-    h2 {
-      font-size: 1.5rem;
-    }
-    h3 {
-      font-size: 1.25rem;
-    }
-    a {
-      color: var(--c--theme--colors--greyscale-500);
-      cursor: pointer;
-    }
-    .bn-block-group
-      .bn-block-group
-      .bn-block-outer:not([data-prev-depth-changed]):before {
-      border-left: none;
-    }
-  }
+const model = createOpenAI({
+  baseURL: 'https://albert.api.staging.etalab.gouv.fr/v1',
+  ...blocknoteAIClient.getProviderSettings('albert-etalab'),
+  compatibility: 'compatible',
+})('albert-etalab/neuralmagic/Meta-Llama-3.1-70B-Instruct-FP8');
 
-  .bn-editor {
-    color: var(--c--theme--colors--greyscale-700);
-  }
+// We call the model via a proxy server (see above) that has the API key,
+// but we could also call the model directly from the frontend.
+// i.e., this should work as well (but it would leak your albert key to the frontend):
+/*
+    return createOpenAI({
+    baseURL: 'https://albert.api.staging.etalab.gouv.fr/v1',
+    apiKey: 'ALBERT-API-KEY',
+    compatibility: 'compatible',
+  })('albert-etalab/neuralmagic/Meta-Llama-3.1-70B-Instruct-FP8');
+*/
 
-  .bn-block-outer:not(:first-child) {
-    &:has(h1) {
-      padding-top: 32px;
-    }
-    &:has(h2) {
-      padding-top: 24px;
-    }
-    &:has(h3) {
-      padding-top: 16px;
-    }
-  }
-
-  & .bn-inline-content code {
-    background-color: gainsboro;
-    padding: 2px;
-    border-radius: 4px;
-  }
-
-  @media screen and (width <= 560px) {
-    & .bn-editor {
-      ${readonly && `padding-left: 10px;`}
-    }
-    .bn-side-menu[data-block-type='heading'][data-level='1'] {
-      height: 46px;
-    }
-    .bn-side-menu[data-block-type='heading'][data-level='2'] {
-      height: 40px;
-    }
-    .bn-side-menu[data-block-type='heading'][data-level='3'] {
-      height: 40px;
-    }
-    & .bn-editor h1 {
-      font-size: 1.6rem;
-    }
-    & .bn-editor h2 {
-      font-size: 1.35rem;
-    }
-    & .bn-editor h3 {
-      font-size: 1.2rem;
-    }
-    .bn-block-content[data-is-empty-and-focused][data-content-type='paragraph']
-      .bn-inline-content:has(> .ProseMirror-trailingBreak:only-child)::before {
-      font-size: 14px;
-    }
-  }
-`;
+export type DocsBlockNoteEditor = BNEditor<
+  Record<string, BlockConfig>,
+  InlineContentSchema,
+  StyleSchema
+>;
 
 interface BlockNoteEditorProps {
   doc: Doc;
@@ -144,30 +92,6 @@ export const BlockNoteEditor = ({ doc, provider }: BlockNoteEditorProps) => {
   const collabName = readOnly
     ? 'Reader'
     : userData?.full_name || userData?.email || t('Anonymous');
-
-  const model = useMemo(() => {
-    const blocknoteAIClient = createBlockNoteAIClient({
-      apiKey: 'BLOCKNOTE-API-KEY-CURRENTLY-NOT-NEEDED',
-      baseURL: 'https://blocknote-esy4.onrender.com/ai',
-    });
-
-    return createOpenAI({
-      baseURL: 'https://albert.api.staging.etalab.gouv.fr/v1',
-      ...blocknoteAIClient.getProviderSettings('albert-etalab'),
-      compatibility: 'compatible',
-    })('albert-etalab/neuralmagic/Meta-Llama-3.1-70B-Instruct-FP8');
-
-    // We call the model via a proxy server (see above) that has the API key,
-    // but we could also call the model directly from the frontend.
-    // i.e., this should work as well (but it would leak your albert key to the frontend):
-    /*
-      return createOpenAI({
-      baseURL: 'https://albert.api.staging.etalab.gouv.fr/v1',
-      apiKey: 'ALBERT-API-KEY',
-      compatibility: 'compatible',
-    })('albert-etalab/neuralmagic/Meta-Llama-3.1-70B-Instruct-FP8');
-     */
-  }, []);
 
   const editor = useCreateBlockNote(
     {
@@ -210,7 +134,7 @@ export const BlockNoteEditor = ({ doc, provider }: BlockNoteEditorProps) => {
       },
       dictionary: {
         ...(locales[lang as keyof typeof locales] as Dictionary),
-        ai: (aiLocales as any)[lang] as Dictionary,
+        ai: aiLocales[lang as keyof typeof aiLocales] as unknown as Dictionary,
       },
       uploadFile,
     },
@@ -251,30 +175,32 @@ export const BlockNoteEditor = ({ doc, provider }: BlockNoteEditorProps) => {
       >
         <BlockNoteAIContextProvider
           model={model}
-          dataFormat={'markdown'}
+          dataFormat="markdown"
           stream={false}
         >
           <BlockNoteAIUI />
           <BlockNoteToolbar />
-          <SuggestionMenu editor={editor} />
+          <SuggestionMenu editor={editor as unknown as DocsBlockNoteEditor} />
         </BlockNoteAIContextProvider>
       </BlockNoteView>
     </Box>
   );
 };
 
-function SuggestionMenu(props: { editor: BNEditor<any, any, any> }) {
+function SuggestionMenu(props: { editor: DocsBlockNoteEditor }) {
   const ctx = useBlockNoteAIContext();
   return (
     <SuggestionMenuController
       triggerCharacter="/"
       getItems={async (query) =>
-        filterSuggestionItems(
-          [
-            ...getDefaultReactSlashMenuItems(props.editor),
-            ...getAISlashMenuItems(props.editor, ctx),
-          ],
-          query,
+        Promise.resolve(
+          filterSuggestionItems(
+            [
+              ...getDefaultReactSlashMenuItems(props.editor),
+              ...getAISlashMenuItems(props.editor, ctx),
+            ],
+            query,
+          ),
         )
       }
     />

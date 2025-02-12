b7914dfeef38b0b623fa4fabaeb3fa8784bde746
Arnaud Robin <arnaud.robin@beta.gouv.fr>
2025-02-12 15:42:10 +0100

    WIP

--- src/backend/core/services/ai_services.py
+++ src/backend/core/services/ai_services.py
@@ -132,7 +132,7 @@ class AIService:
             )
             # pdf_content = pdf_response['Body'].read()
             
-            # Call the Albert / Mistral API
+            # # Call the Albert / Mistral API
             # api_url = f"{settings.ACCESSIBILITY_API_BASE_URL}/transcribe/pdf"
             # files = {'pdf': ('document.pdf', pdf_content)}
             
@@ -141,8 +141,85 @@ class AIService:
             
             # transcribed_text = response.json()['text']
 
-            transcribed_text = "Hello world"
-            
+            transcribed_text = """
+## Page 1
+
+# 13 FÉVR 2019
+
+## DIRECTION GÉNÉRALE DES FINANCES PUBLIQUES
+
+### SERVICE DES RETRAITES DE L'ÉTAT
+
+BUREAU FINANCIER ET DES STATISTIQUES
+
+**Adresse :**10 rue de la Paix75572 Paris Cedex 12
+
+**Téléphone :**01 44 94 90 00
+
+**N° DF : 4BRB-19-8351**
+
+**NOR : CPAB1833933C**
+
+**Le Ministre de l'Action et des Comptes publics**
+
+**à**
+
+**Mésdames et Messieurs les Ministres et Secrétaires d'État**
+
+**Objet : Nomenclature commentée des recettes du CAS Pensions – année 2019.**
+
+Le compte d'affectation spéciale (CAS) Pensions constitue une mission au sens de la LOLF et comporte trois programmes détaillés :
+
+*   **le programme 741** « pensions civiles et militaires de retraite et allocations temporaires d'invalidité » qui est le principal programme de cette mission en termes d'eneux financiers ;
+*   **le programme 742** « ouvriers des établissements industriels de l'État » qui retrace la gestion par la Caisse des dépôts et consignations du fonds spécial des pensions des ouvriers des établissements industriels de l'État (FSPOEIE) et des établissements militaires (FATOCEM) ;
+*   **le programme 743** « pensions militaires d'invalidité et des victimes de guerre et autres pensions » dont les dépenses sont prises en charge par la solidarité nationale et financières par des versements du budget général.
+
+La présente circulaire a pour objet d'informer les acteurs du CAS Pensions, ordonnateurs et comptables, du contenu de chacune des lignes de la nomenclature budgétaire et comptable retenue pour l'année 2019. La bonne imputation des recettes sur les lignes et comptes budgétaires ouverts à la nomenclature est une étape clé du fonctionnement du CAS Pensions dans la mesure où elle engage les opérations de contrôle et suivi opérées pour les recettes.
+
+**MINISTÈRE DE L'ACTION**
+
+**ET DES COMPTES PUBLICS**
+
+## Page 2
+
+Par rapport à la nomenclature budgétaire et comptable 2018, la version 2019 est complète des lignes de recettes des programmes 742 et 743.
+
+Afin de tenir compte de l'entrée en vigueur des décrets relatifs aux versements et aux obligations déclaratives des employeurs, deux nouveaux comptes budgétaires ont également été créés : un pour permettre la comptabilisation des éventuelles majorations de retard (781 693) et des pénalités employeurs (781 694).
+
+L'article 2 de la loi du 24 décembre 2018 instaure une exonération de cotisations sociales sur les heures supplémentaires effectuées à compter du 1er janvier 2019. Ces heures supplémentaires seront également prises en compte pour le revenu dans une limite annuelle égale à 5 000 €. La nomenclature 2019 intègre deux nouveaux comptes budgétaires pour permettre l'imputation au CAS Pensions de cette mesure pour les collecteurs des personnels civils (781 013) et militaires (781 113).
+
+La nomenclature actualisée les taux applicables en 2019 pour la programme 741 :
+
+|                                                                |               |
+| -------------------------------------------------------------- | ------------- |
+| **Cotisation pour pensions**                                   | **Taux 2029** |
+| Intensité pour pension agent                                   | 50,85 %       |
+| Contribution employeur - personnel civil                       | 74,24 %       |
+| Contribution employeur - personnel militaire                   | 146,07 %      |
+| Contribution employeur - ATI                                   | 0,32 %        |
+| Surcotation : taux supplémentaire de la contribution employeur | 30,65 %       |
+
+Je vous remercie de bien vouloir diffuser cette circulaire auprès de vos services compétents.
+
+Pour le Ministre, la Directrice du Budget. Par délégation, la Sous-Directrice
+
+Marie CHANCHOLE
+
+Pour le Ministre, le Directeur généraldes Finances publiquesPar délégation, le Directeur du servicedes retraites de l'État
+
+Jean-Pierre MAU
+
+### Réponses :
+
+1.  Commenter des différentes lignes de recettes des trois programmes du CAS Pensions et deux fiches schématiques (horus).
+2.  Abréviation précisant l'imputation comptable sur les comptes budgétaires CHORUS pour la plupart des études de fonctionnaires.
+3.  Définition des termes couramment utilisés dans la nomenclature.
+
+Décret n° 2018-873 du 10 octobre 2018 relatif aux modalités et contributions pour les pensions et allocations temporaires d'invalidité et aux obligations déclaratives pour les comptes individuels de retraite, des magistrats et militaires.
+
+Le décret du 10 octobre 2018 modifie les modalités et contributions pour les pensions et allocations temporaires d'invalidité et aux obligations déclaratives pour les comptes individuels de retraite, des fonctionnaires de l'État, des magistrats et militaires. Il permet de prendre en compte les modifications apportées par la loi pour la conservation des pensions des fonctionnaires de l'État, des magistrats et militaires, afin de permettre des contributions et des cotisations et aux obligations déclaratives pour les comptes individuels de retraite, des fonctionnaires de l'État, des magistrats et militaires.
+
+            """
             return transcribed_text
         except Exception as e:
             raise RuntimeError(f"Failed to transcribe PDF: {str(e)}")

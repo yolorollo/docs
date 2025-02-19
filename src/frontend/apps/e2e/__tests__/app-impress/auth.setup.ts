ad47fc2d60e0fe97f9246705459c7639e0e6551c
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2025-02-19 12:50:51 +0100

    ✅(e2e) global setup authentication
    Because of the parallelism of the tests,
    the authentication setup was flaky. Sometimes
    the tests would run before the authentication
    was complete.
    We change to a global setup instead of the
    project dependency setup, it should be more
    reliable.
    We improved the waiting states of the authentication
    setup.

--- src/frontend/apps/e2e/__tests__/app-impress/auth.setup.ts
+++ src/frontend/apps/e2e/__tests__/app-impress/auth.setup.ts
@@ -1,27 +1,46 @@
-import { test as setup } from '@playwright/test';
+import { FullConfig, FullProject, chromium, expect } from '@playwright/test';
 
 import { keyCloakSignIn } from './common';
 
-setup('authenticate-chromium', async ({ page }) => {
-  await page.goto('/');
-  await keyCloakSignIn(page, 'chromium');
-  await page
-    .context()
-    .storageState({ path: `playwright/.auth/user-chromium.json` });
-});
+const saveStorageState = async (
+  browserConfig: FullProject<unknown, unknown>,
+) => {
+  const browserName = browserConfig?.name || 'chromium';
 
-setup('authenticate-webkit', async ({ page }) => {
-  await page.goto('/');
-  await keyCloakSignIn(page, 'webkit');
-  await page
-    .context()
-    .storageState({ path: `playwright/.auth/user-webkit.json` });
-});
+  const { storageState, ...useConfig } = browserConfig?.use;
+  const browser = await chromium.launch();
+  const context = await browser.newContext(useConfig);
+  const page = await context.newPage();
 
-setup('authenticate-firefox', async ({ page }) => {
-  await page.goto('/');
-  await keyCloakSignIn(page, 'firefox');
-  await page
-    .context()
-    .storageState({ path: `playwright/.auth/user-firefox.json` });
-});
+  await page.goto('/', { waitUntil: 'networkidle' });
+  await page.content();
+  await expect(page.getByText('Docs').first()).toBeVisible();
+
+  await keyCloakSignIn(page, browserName);
+
+  await expect(
+    page.locator('header').first().getByRole('button', {
+      name: 'Logout',
+    }),
+  ).toBeVisible();
+
+  await page.context().storageState({
+    path: storageState as string,
+  });
+
+  await browser.close();
+};
+
+async function globalSetup(config: FullConfig) {
+  /* eslint-disable @typescript-eslint/no-non-null-assertion */
+  const chromeConfig = config.projects.find((p) => p.name === 'chromium')!;
+  const firefoxConfig = config.projects.find((p) => p.name === 'firefox')!;
+  const webkitConfig = config.projects.find((p) => p.name === 'webkit')!;
+  /* eslint-enable @typescript-eslint/no-non-null-assertion */
+
+  await saveStorageState(chromeConfig);
+  await saveStorageState(webkitConfig);
+  await saveStorageState(firefoxConfig);
+}
+
+export default globalSetup;

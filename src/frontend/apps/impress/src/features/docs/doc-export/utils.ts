51cc26b91685b280a0f32a468c7ea6c2be9250f3
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2025-03-19 13:08:38 +0100

    🐛(frontend) improve svg export to be less pixelized
    Some SVGs were pixelized in the exported files.
    We now add the wanted size to the svg conversion to
    make sure the images are exported with the correct size
    and so less pixelized.

--- src/frontend/apps/impress/src/features/docs/doc-export/utils.ts
+++ src/frontend/apps/impress/src/features/docs/doc-export/utils.ts
@@ -23,16 +23,37 @@ export function downloadFile(blob: Blob, filename: string) {
  * @param svgText - The SVG text to convert
  * @returns The PNG data URL
  */
-export async function convertSvgToPng(svgText: string) {
+export async function convertSvgToPng(svgText: string, width: number) {
   // Create a canvas and render the SVG onto it
   const canvas = document.createElement('canvas');
-  const ctx = canvas.getContext('2d');
+  const ctx = canvas.getContext('2d', {
+    alpha: true,
+  });
 
   if (!ctx) {
     throw new Error('Canvas context is null');
   }
 
+  // Parse SVG to get original dimensions
+  const parser = new DOMParser();
+  const svgDoc = parser.parseFromString(svgText, 'image/svg+xml');
+  const svgElement = svgDoc.documentElement;
+
+  // Get viewBox or fallback to width/height attributes
+  let height;
+  const svgWidth = svgElement.getAttribute?.('width');
+  const svgHeight = svgElement.getAttribute?.('height');
+  const viewBox = svgElement.getAttribute('viewBox')?.split(' ').map(Number);
+
+  const originalWidth = svgWidth ? parseInt(svgWidth) : viewBox?.[2];
+  const originalHeight = svgHeight ? parseInt(svgHeight) : viewBox?.[3];
+  if (originalWidth && originalHeight) {
+    const aspectRatio = originalHeight / originalWidth;
+    height = Math.round(width * aspectRatio);
+  }
+
   const svg = Canvg.fromString(ctx, svgText);
+  svg.resize(width, height, true);
   await svg.render();
 
   return canvas.toDataURL('image/png');

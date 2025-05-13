fc9cda9d1eda49c363a4f0eff93c1f12e5e78792
Samuel Paccoud - DINUM <samuel.paccoud@mail.numerique.gouv.fr>
2025-05-13 17:58:45 +0200

    ✨(backend) add max_role field to the document access API endpoint
    The frontend needs to know what to display on an access. The maximum
    role between the access role and the role equivalent to all accesses
    on the document's ancestors should be computed on the backend.

--- src/backend/core/api/serializers.py
+++ src/backend/core/api/serializers.py
@@ -304,6 +304,7 @@ class DocumentAccessSerializer(serializers.ModelSerializer):
     team = serializers.CharField(required=False, allow_blank=True)
     abilities = serializers.SerializerMethodField(read_only=True)
     max_ancestors_role = serializers.SerializerMethodField(read_only=True)
+    max_role = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = models.DocumentAccess
@@ -317,8 +318,15 @@ class DocumentAccessSerializer(serializers.ModelSerializer):
             "role",
             "abilities",
             "max_ancestors_role",
+            "max_role",
         ]
-        read_only_fields = ["id", "document", "abilities", "max_ancestors_role"]
+        read_only_fields = [
+            "id",
+            "document",
+            "abilities",
+            "max_ancestors_role",
+            "max_role",
+        ]
 
     def get_abilities(self, instance) -> dict:
         """Return abilities of the logged-in user on the instance."""
@@ -330,6 +338,13 @@ class DocumentAccessSerializer(serializers.ModelSerializer):
     def get_max_ancestors_role(self, instance):
         """Return max_ancestors_role if annotated; else None."""
         return getattr(instance, "max_ancestors_role", None)
+
+    def get_max_role(self, instance):
+        """Return max_ancestors_role if annotated; else None."""
+        return choices.RoleChoices.max(
+            getattr(instance, "max_ancestors_role", None),
+            instance.role,
+        )
 
     def update(self, instance, validated_data):
         """Make "user" field readonly but only on update."""
@@ -354,6 +369,7 @@ class DocumentAccessLightSerializer(DocumentAccessSerializer):
             "role",
             "abilities",
             "max_ancestors_role",
+            "max_role",
         ]
         read_only_fields = [
             "id",
@@ -362,6 +378,7 @@ class DocumentAccessLightSerializer(DocumentAccessSerializer):
             "role",
             "abilities",
             "max_ancestors_role",
+            "max_role",
         ]
 
 
